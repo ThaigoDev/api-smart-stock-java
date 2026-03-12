5f1e6c83a0b3db6df66a9abb445cdd856ebc504e
ThaigoDev <thaigodeveloper@gmail.com>
2026-03-12 16:36:27 -0300

    autorizeConfiguration

--- src/main/java/com/thai/finance/api/finance/api/services/ServiceClienteSeguranca.java
+++ src/main/java/com/thai/finance/api/finance/api/services/ServiceClienteSeguranca.java
@@ -5,7 +5,11 @@ import com.thai.finance.api.finance.api.domain.dtos.ClienteSegurancaDTO.ClienteS
 import com.thai.finance.api.finance.api.mapper.MapperClienteSeguranca;
 import com.thai.finance.api.finance.api.respository.RepositoryClienteSeguranca;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -17,4 +21,9 @@ public class ServiceClienteSeguranca {
         return  mapper.paraDTO(repositoryClienteSeguranca.save(mapper.paraEntidade(clienteSegurancaRequisicaoDTO))) ;
     }
 
+    public ClienteSegurancaRespostaDTO obterPorId(String clientId) {
+      return mapper.paraDTO(repositoryClienteSeguranca
+              .findById(UUID.fromString(clientId))
+              .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado")));
+    }
 }
